ad213d7794c73e8f679c88a91f03c9de4ec0d9e1
AllanRBonato <allan-bonato@hotmail.com>
2026-03-16 20:58:24 -0300

    Atualização do curso

---
+++ jsBasico/tiposPrimitivo.js
@@ -0,0 +1,23 @@
+const nome = 'Allan';   // String
+const nome1 = "Allan";  // String
+const nome2 = `Allan`;  // String
+const num1 = 10;        // number
+const num2 = 10.52;     // number
+let nomeAluno;          // underfined -> não aponta para local nenhum da memória
+const sobrenomeAluno = null;    // Nulo -> não aponta para local nenhum na memória
+const aprovado = false;          // Boolean = true, false (lógico)
+
+console.log(typeof nome);
+console.log(typeof num1);
+console.log(typeof nomeAluno);
+console.log(typeof sobrenomeAluno);
+console.log(typeof aprovado);
+
+
+/*
+    String      - Texto ou palavra 
+    number      - Número
+    underfined  - Não aponta local na memória
+    Null        - Não aponta local na memória
+    Boolean     - Verdadeiro ou Falso
+*/
