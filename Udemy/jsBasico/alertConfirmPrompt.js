8cad19ced5966e168cc0742bd7736ee54bba3535
AllanRBonato <allan-bonato@hotmail.com>
2026-04-04 22:45:14 -0300

    atualização

--- Udemy/jsBasico/alertConfirmPrompt.js
+++ Udemy/jsBasico/alertConfirmPrompt.js
@@ -1,1 +1,8 @@
 // quando uma função esta dentro de um objeto chama de metodo, quando ta fora chama são funções.
+
+const num1 = Number(prompt('Digite um número: '));
+const num2 = Number(prompt('Digite um número: '));
+
+const soma = num1 + num2;
+
+alert(`A soma dos dois número é: ${soma}`);
