c54033758934e5c2f10e4b1a989e8f24134700b8
AllanRBonato <allan-bonato@hotmail.com>
2026-04-22 09:29:36 -0300

    atualização de estudos

---
+++ Udemy/jsBasico/objetoMath.js
@@ -0,0 +1,14 @@
+// let num1 = 9;
+// // console.log(num1 ** (1/2)); // Raiz quadrada // Ou console.log(num1 ** 0.5);
+// let num2 = Math.floor(num1); // Arredonda para baixo
+// let num2 = Math.ceil(num1); // Arredonda para cima
+// let num2 = Math.round(num1) // Arredonda para o mais próximo
+// console.log(num2);
+// console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6)); // Retornar o maior número da sequencia
+// console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6)); // Retornar o menor número da sequencia
+// const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+// console.log(aleatorio);
+// console.log(Math.pow(2, 10)); // Ou console.log(2 ** 10);
+
+// // Erro no js que pode dividir um número por 0:
+// console.log(100 / 0); // Não da erro e é avaliado como verdadeiro => retorna como infinity
