16b6b07d1f6b3db13a13e47782af0c13b64ef8df
AllanRBonato <allan-bonato@hotmail.com>
2026-04-23 10:12:31 -0300

    atilazaçaõ

---
+++ Udemy/jsBasico/testeArrayConst.js
@@ -0,0 +1,11 @@
+const lista = [1, 2, 3 , 4];
+lista[2] = 'allan';
+console.log(lista);
+
+
+/*
+No caos consigo retribuir a informação dentro do arrya, no caso consigo "trocar" os elemento deles, não consigo reatribuir a informação da váriavel, que é o proprio array.
+*/
+// como nesse caso, como não tem indice ele esta tendo funcionar como o lete, colocando o nome da variável para tentar reatrubuir o dado da vairável, o que não pode.
+const array = [1, 2, 3, 4, 5];
+array = 'Legal';
