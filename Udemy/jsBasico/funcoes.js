ac34fd5676f60aad80a1f61da209a757f243b8ae
AllanRBonato <allan-bonato@hotmail.com>
2026-04-24 10:52:30 -0300

    atilazaçaõ

---
+++ Udemy/jsBasico/funcoes.js
@@ -0,0 +1,8 @@
+// nome de funções é a mesma nome de nomear as variáveis.
+
+// Quando declarado uma função da forma normal, como no exemplo em baixo, não precisa colocar ; no fim dos }.
+function saudacao(nome) {
+    console.log(`Bom dia ${nome}`);
+}
+
+saudacao("allan");
