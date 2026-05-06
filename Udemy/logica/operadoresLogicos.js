f1f06de32ec24e1c2498c3de5a51d20dd853c997
AllanRBonato <allan-bonato@hotmail.com>
2026-05-05 21:07:42 -0300

    atualiação

---
+++ Udemy/logica/operadoresLogicos.js
@@ -0,0 +1,29 @@
+/*
+Operadores Lógicos
+
+&& -> AND -> e -- Todas as expressões precisam ser verdadeiras para retornar true.
+|| -> OR  -> ou
+!  -> NOT -> não
+*/
+
+// const expressao = true && true & true; // Verdadeiro
+// const expressao = true && true & false; // Falso
+// console.log(expressao);
+
+// const expressaoAnd = true || false; // Retorna a expressão como verdadeira, mesmo tendo um false.
+// console.log(expressaoAnd);
+
+
+// Exemplo de comparação com mais tipos de operadores:
+// const usuario = 'Allan';
+// const senha = 16081995;
+
+// const vaiLogar = usuario === 'Allan' && senha === 16081995;
+// console.log(vaiLogar);
+
+
+// Exemplo de negação NOR (!):
+// Pode usar várias vezes, ai vai mudando o tipo cada vez que foi colocado.
+// console.log(!false); // Retorna true.
+// console.log(!true); // Retorna false
+
