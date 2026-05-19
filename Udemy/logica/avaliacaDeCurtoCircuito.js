02960315a54aaa1b8c839ae0e2897a9beb3b5085
AllanRBonato <allan-bonato@hotmail.com>
2026-05-19 09:57:17 -0300

    atualização de curso

---
+++ Udemy/logica/avaliacaDeCurtoCircuito.js
@@ -0,0 +1,27 @@
+/*
+
+&& - false and true = false // Ou seja ambas precisam ser verdadeiras ou falsa para ser true - se um for errado ele para o processo - o valor mesmo 
+|| - true || false -> vai retornar o vcalor verdadeiro, qualquer uma pode ser verdadeiro para funcionar.
+
+*/
+
+// console.log(0 || false || null || 'Allan Bonato' || true); // Nesse caos retornar o primeiro verdadeiro, uma string é considerado verdadeiro?
+
+// console.log('Allan Bonato' && 0 && 'Maria'); // Nesse caso retorna o valor mesmo, no caso é o 0; e se ele não encontra nada falso, ele retrona o último valor.
+// nesse caso a cima é um valor falso pq tem algum que é diferente do outros padrões, considerando os valores falsos mostrado em baixo.
+
+/*
+
+Os valors flasos em js: 
+false
+0
+' '
+" "
+` `
+null
+underfined
+NaN
+
+*/
+
+
