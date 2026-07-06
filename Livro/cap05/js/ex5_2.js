4a217c67085255ad5c80ad2a0cdc53ce28e4f804
allan.bonato <allan-bonato@hotmail.com>
2026-07-06 17:59:48 -0300

    Atualização

---
+++ Livro/cap05/js/ex5_2.js
@@ -0,0 +1,16 @@
+const form = document.querySelector('form');
+const resp = document.querySelector('h3');
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const numero = Number(form.numero.value);
+
+    let resposta = `Entre ${numero} e 1: `
+
+    for(let i = numero; i > 1; i--) {
+        resposta = resposta + i + ', '
+    }
+    resp.innerText = resposta + '1 ';
+})
