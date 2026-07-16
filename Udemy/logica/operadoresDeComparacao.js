eb3544556810bef16dfd15ef443b8c297bfc8204
allan.bonato <allan-bonato@hotmail.com>
2026-07-16 19:24:08 -0300

    Atualização

---
+++ Udemy/logica/operadoresDeComparacao.js
@@ -0,0 +1,12 @@
+/* 
+    Operadores de comparação:
+    > Maior que
+    >= Maior que ou igual a 
+    < Menor que 
+    <= Menor que ou igual a 
+    == Igualdade
+    === Igauldade estrita (valor e tipo)
+    != difernete
+    !== dirferente estrito (valor e tipo)
+*/
+
