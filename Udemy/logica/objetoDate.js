7b15e9565f0fc01901d52bb1464f1ba89b943670
allan.bonato <allan-bonato@hotmail.com>
2026-08-06 16:32:29 -0300

    atualização

--- Udemy/logica/objetoDate.js
+++ Udemy/logica/objetoDate.js
@@ -4,5 +4,35 @@
 // const data = new Date(0 + tresHoras + umDia); // 01/01/1970 Timestamp unix ou época unix
 //                    ano   mês  dia   hora  minutos   milesimos
 // const data = new Date(2026,  7,   3,    20,    32,        27); // o mês começa com 0 - mandaria: ano/ mês/ dia/ hora/ minutos/ segundos/ milissegundo
-const data = new Date('2026-08-03 20:44:59');
-console.log(data.toString());
+// const data = new Date(1786018063018);
+// console.log('Dia', data.getDate());
+// console.log('Mês', data.getMonth()); // Mês começa do zero
+// console.log('Ano', data.getFullYear());
+// console.log('Hora', data.getHours());
+// console.log('Min', data.getMinutes());
+// console.log('Seg', data.getSeconds());
+// console.log('ms', data.getMilliseconds());
+// console.log('Dia da semana', data.getDay()); // 0 - Domingi, 6 - sábado
+// console.log(data.toString());
+// console.log(Date.now());
+
+
+function zeroAEsquerda (num) {
+    return num >= 10 ? num : `0${num}`;
+}
+
+
+function formataData(data) {
+    const dia = zeroAEsquerda(data.getDate());
+    const mes = zeroAEsquerda(data.getMonth() + 1);
+    const ano = zeroAEsquerda(data.getFullYear());
+    const hora = zeroAEsquerda(data.getHours());
+    const min = zeroAEsquerda(data.getMinutes());
+    const seg = zeroAEsquerda(data.getSeconds());
+
+    return `${dia}/${mes}/${ano} - ${hora}:${min}:${seg}`;
+}
+
+const data = new Date();
+const dataBrasil = formataData(data);
+console.log(dataBrasil);
