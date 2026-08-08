0e67886e61294fac5cb0a873e6c389ad3577556e
allan.bonato <allan-bonato@hotmail.com>
2026-08-08 18:45:21 -0300

    atualização

--- Udemy/logica/switchDate.js
+++ Udemy/logica/switchDate.js
@@ -1,4 +1,19 @@
-const data = new Date();
+const prompt = require('prompt-sync')();
+
+const ano = Number(prompt('Digite o ano: '));
+const mes = Number(prompt('Digite o mês: '));
+const dia = Number(prompt('Digite o dia: '));
+
+
+
+const data = new Date(ano, mes - 1, dia);
 const diaSemana = data.getDay();
 
-console.log(diaSemana);
+function obeterDiaSemana() {
+    const diasDaSemana = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+
+    return diasDaSemana[diaSemana];
+}
+
+
+console.log(data, diaSemana, obeterDiaSemana());
