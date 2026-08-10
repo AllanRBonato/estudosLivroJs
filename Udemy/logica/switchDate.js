97c8b29e7391b9107bc75999e74ef354a54c7913
allan.bonato <allan-bonato@hotmail.com>
2026-08-10 17:15:29 -0300

    atualização

--- Udemy/logica/switchDate.js
+++ Udemy/logica/switchDate.js
@@ -7,13 +7,40 @@ const dia = Number(prompt('Digite o dia: '));
 
 
 const data = new Date(ano, mes - 1, dia);
-const diaSemana = data.getDay();
+let diaSemana = data.getDay();
+let diaSemanaTexto
 
-function obeterDiaSemana() {
-    const diasDaSemana = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+// function obeterDiaSemana() {
+//     const diasDaSemana = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
 
-    return diasDaSemana[diaSemana];
+//     return diasDaSemana[diaSemana];
+// }
+
+
+// console.log(data, diaSemana, obeterDiaSemana());
+
+switch (diaSemana) {
+    case 0:
+        diaSemanaTexto = 'Domingo';
+    break;
+    case 1:
+        diaSemanaTexto = 'Segunda';
+    break;
+    case 2:
+        diaSemanaTexto = 'Terça';
+    break;
+    case 3:
+        diaSemanaTexto = 'Quarta';
+    break;
+    case 4:
+        diaSemanaTexto = 'Quinta';
+    break;
+    case 5:
+        diaSemanaTexto = 'Sexta';
+    break;
+    case 6:
+        diaSemanaTexto = 'Sábado';
+    break;
 }
 
-
-console.log(data, diaSemana, obeterDiaSemana());
+console.log(diaSemana, diaSemanaTexto);
