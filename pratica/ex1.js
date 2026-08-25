af5a6dd84c150369e2d233198941a1c9ea82607b
allan.bonato <allan-bonato@hotmail.com>
2026-08-25 17:09:32 -0300

    atualização

--- pratica/ex1.js
+++
@@ -1,9 +0,0 @@
-const prompt = require('prompt-sync')()
-
-const velocidade = Number(prompt('Digite a velocidade do condutor: '));
-
-if (velocidade > 80) {
-    console.log('Você foi multado por excesso de velocidade!')
-} else {
-    console.log('Velocidade dentro do limite permitido.')
-}
---
+++ pratica/for1.js
@@ -0,0 +1,25 @@
+/*
+
+o for tem 3 passos 
+
+1 - a partida:
+
+let i = 0;
+
+
+2 - a condição para continuar:
+
+i < 3;
+
+
+3 - o que incrimenta ou decrementa:
+
+i++
+
+*/
+
+const prompt = require('prompt-sync')();
+
+const totalAssentos = Number(prompt('Quantos assentos tem a fileira: '));
+
+
